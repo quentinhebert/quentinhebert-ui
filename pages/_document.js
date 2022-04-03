6f8c96df1ff4a471363044f2897f97b86770f88b
Quentin HEBERT <tetedetryso@gmail.com>
2022-04-04 00:12:07 +0200

    fix errors in front due to bad insertion of styles

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <link href="/styles/globals.css" rel="stylesheet"></link>
-      </Head>
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
