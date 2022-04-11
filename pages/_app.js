6983105d7b7dcf42a395711a624b421b6e60027f
Quentin HEBERT <tetedetryso@gmail.com>
2022-04-11 19:16:00 +0200

    fix app not getting user before rendering an authenticated page if user comes from URL and not app navigation

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,37 @@
-import { ThemeProvider } from '@mui/material';
-import '../styles/globals.css';
-import theme from '../config/theme';
+import { ThemeProvider } from "@mui/material";
+import "../styles/globals.css";
+import theme from "../config/theme";
+import { useState } from "react";
+import { UserContext } from "../contexts/UserContext";
+import { getToken } from "../services/cookies";
+import { getUser } from "../services/utils";
+import apiCall from "../services/apiCalls/apiCall";
 
 function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useState(null);
+  const [accessToken, setAccessToken] = useState(getToken());
+
+  // In case we have to convert access token into a user (i.e. when coming directly from URL)
+  const fetchUser = async () => {
+    const userFromToken = getUser(accessToken);
+    const res = await apiCall.users.get(userFromToken.id);
+    if (res && res.ok) {
+      const userData = await res.json();
+      setUser(userData);
+      return userData;
+    }
+  };
+  if (!user && !!accessToken && accessToken !== "") {
+    fetchUser();
+    return <></>;
+  }
+
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UserContext.Provider value={{ user, setUser, setAccessToken }}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UserContext.Provider>
   );
 }
 
