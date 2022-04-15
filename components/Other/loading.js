82e75f85e1f884006487da5603a340fac4fb0808
Quentin HEBERT <tetedetryso@gmail.com>
2022-04-15 15:50:02 +0200

    rework loading page

--- components/Other/loading.js
+++ components/Other/loading.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
 import theme from "../../config/theme";
+import { Stack } from "@mui/material";
 
 export default function Loading() {
   return (
@@ -13,7 +14,13 @@ export default function Loading() {
         zIndex: (theme) => theme.zIndex.drawer + 100,
       }}
     >
-      <CircularProgress color="inherit" />
+      <Stack
+        justifyContent="center"
+        alignItems="center"
+        sx={{ width: "100%", height: "100vh", backgroundColor: "#000" }}
+      >
+        <CircularProgress sx={{ color: "#fff" }} />
+      </Stack>
     </Backdrop>
   );
 }
