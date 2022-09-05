5b50aefbfb3089ef3daf41f89f836ee8db763fce
Quentin HEBERT <hebertquentin@aol.fr>
2022-09-05 13:12:56 +0200

    first shot for website page

---
+++ components/Layouts/websites/WebsiteFocusPart.js
@@ -0,0 +1,127 @@
+import { useEffect, useState } from "react"
+import { Box, Button, Slide, Stack } from "@mui/material"
+import BigTitle from "../../ReusableComponents/titles/big-title"
+import BodyText from "../../ReusableComponents/text/body-text"
+import { useAnimation, motion } from "framer-motion"
+import { useInView } from "react-intersection-observer"
+import CenteredMaxWidthContainer from "../../ReusableComponents/containers/centered-max-width-container"
+
+const Keyword = ({ text }) => (
+  <Box
+    component="span"
+    sx={{
+      color: (theme) => theme.palette.text.secondary,
+      fontSize: { xs: "1.2rem", md: "1.5rem" },
+      fontWeight: "bold",
+      fontStyle: "italic",
+    }}
+  >
+    {text}
+  </Box>
+)
+
+export default function WebsiteFocusPart(props) {
+  const { refsForScroll } = props
+
+  const scrollTo = (ref) => {
+    ref.current.scrollIntoView({
+      behavior: "smooth",
+    })
+  }
+
+  /********** ANIMATION **********/
+  const [ref, inView] = useInView()
+  const [show, setShow] = useState(false)
+  const variants = {
+    visible: {
+      opacity: 1,
+      transition: { duration: 1 },
+    },
+    hidden: { opacity: 0 },
+  }
+  const controls = useAnimation()
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible")
+      setShow(true)
+    } else {
+      controls.start("hidden")
+      setShow(false)
+    }
+  }, [controls, inView])
+
+  const expNbYears = new Date().getFullYear() - 2019 // J'ai réalisé mes premiers clips de musique en 5ème (collège) à l'âge de 13 ans
+
+  return (
+    <>
+      {/* Section Anchor */}
+      <Stack ref={refsForScroll.focus} style={{ scrollMarginTop: "65px" }} />
+
+      <Stack
+        sx={{
+          marginTop: "0.01px",
+          padding: "4rem",
+          background: (theme) =>
+            `linear-gradient(360deg, ${theme.palette.background.main} 0%, rgb(0,0,0,1) 60%)`,
+        }}
+        ref={ref}
+      >
+        <motion.div initial="hidden" variants={variants} animate={controls}>
+          <Stack width="100%" alignItems="center">
+            <BigTitle
+              title="Full-Stack ?"
+              sx={{
+                background: (theme) =>
+                  `linear-gradient(${theme.palette.text.secondary} 20%, #000 100%)`,
+                WebkitBackgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                lineHeight: "8rem",
+              }}
+              fontFamily="Zacbel X"
+              textAlign="center"
+            />
+
+            <CenteredMaxWidthContainer>
+              <BodyText
+                textAlign="center"
+                color={(theme) => theme.palette.text.primaryLight}
+                sx={{
+                  margin: "2rem auto",
+                }}
+              >
+                Attiré par le <Keyword text="design graphique" /> et la{" "}
+                <Keyword text="logique algorithmique" />, je deviens développeur
+                web à l'issue de mon diplôme d'
+                <Keyword text="ingénieur informatique" />.
+                <p />
+                J'exerce ce métier depuis maintenant {expNbYears} ans.
+                <p />
+                J'aide les artistes et les entreprises à{" "}
+                <Keyword text="mieux vendre" /> leurs produits et leurs services
+                au travers de <Keyword text="sites qui leur ressemblent" />.
+              </BodyText>
+
+              <Box sx={{ textAlign: "center" }}>
+                <Button
+                  variant="outlined"
+                  color="secondary"
+                  sx={{
+                    fontSize: { xs: "1rem", md: "1.2rem" },
+                    fontWeight: "bold",
+                    letterSpacing: "1.5px",
+                    border: (theme) =>
+                      `1px solid ${theme.palette.secondary.main}`,
+                  }}
+                  onClick={(e) => scrollTo(refsForScroll.portfolio)}
+                >
+                  Voir mes réalisations
+                </Button>
+              </Box>
+            </CenteredMaxWidthContainer>
+          </Stack>
+        </motion.div>
+      </Stack>
+    </>
+  )
+}
--- components/Layouts/websites/WebsitesIndexLayout.js
+++ components/Layouts/websites/WebsitesIndexLayout.js
@@ -1,18 +1,21 @@
-import React, { useEffect, useRef, useState } from "react"
+import { useRef } from "react"
 import Navbar from "../../Navigation/Navbars/navbar"
 import Footer from "../../Navigation/Footers/Footer"
-import { Box, Slide, Stack, Typography, useMediaQuery } from "@mui/material"
+import { useMediaQuery } from "@mui/material"
 import ScrollToTopBtn from "../../Navigation/scroll-to-top"
 import theme from "../../../config/theme"
-import BouncingArrow from "../../Navigation/BouncingArrow"
+import WebsitesIndexHero from "./WebsitesIndexHero"
+import WebsiteFocusPart from "./WebsiteFocusPart"
+import ContactSection from "../../Sections/contact-section"
 
 export default function WebsitesIndexLayout(props) {
   const { refForScroll } = props
 
   const topRef = useRef()
-  const categoriesRef = useRef()
+  const focusRef = useRef()
   const refsForScroll = {
-    portfolio: categoriesRef,
+    top: topRef,
+    focus: focusRef,
   }
   const scrollTo = (ref) => {
     ref.current.scrollIntoView({
@@ -25,110 +28,14 @@ export default function WebsitesIndexLayout(props) {
   return (
     <>
       <Navbar bgColor="transparent" />
-      <Stack
-        sx={{
-          backgroundImage: "url(/medias/folds-background.jpg)",
-          backgroundSize: "cover",
-          backgroundPosition: "50%",
-          backgroundRepeat: "no-repeat",
-          width: "100%",
-          minHeight: "400px",
-          height: { xs: "50vh", md: "100vh" },
-          maxHeight: "900px",
-          alignItems: "end",
-        }}
-      >
-        <Slide direction="left" {...{ timeout: 1000 }} in>
-          <Typography
-            variant="h1"
-            color="secondary"
-            fontFamily="Zacbel X"
-            sx={{
-              textAlign: "right",
-              fontSize: {
-                xs: "3rem",
-                sm: "5.5rem",
-                md: "8.5rem",
-                lg: "10.5rem",
-                xl: "15rem",
-              },
-              lineHeight: {
-                xs: "3.5rem",
-                sm: "6rem",
-                md: "7rem",
-                lg: "10rem",
-                xl: "13rem",
-              },
-              position: "absolute",
-              zIndex: 0,
-              padding: {
-                xs: "7rem 1.5rem 0 1rem",
-                sm: "7rem 1.5rem 0 1rem",
-                md: "7rem 1.5rem 0 5rem",
-              },
-            }}
-          >
-            Developpeur
-            <br />
-            Freelance
-          </Typography>
-        </Slide>
 
-        <Slide
-          direction="right"
-          {...(true ? { timeout: 1000 } : {})}
-          in
-          mountOnEnter
-          unmountOnExit
-        >
-          <Box
-            sx={{
-              position: "absolute",
-              backgroundImage: "url(/medias/developper-alpha.png)",
-              backgroundSize: {
-                xs: "200%",
-                sm: "150%",
-                md: "200%",
-                lg: "150%",
-              },
-              backgroundPosition: {
-                xs: "30% -45%",
-                sm: "30% 20% ",
-                md: "30% 20%",
-                lg: "30% 30%",
-                xl: "0% 0%",
-              },
-              backgroundRepeat: "no-repeat",
-              width: "100%",
-              minHeight: "400px",
-              height: { xs: "50vh", md: "100vh" },
-              maxHeight: "900px",
-              zIndex: 1,
-            }}
-          />
-        </Slide>
+      <WebsitesIndexHero refForScroll={refsForScroll.focus} />
 
-        {!md ? (
-          <Stack
-            zIndex={10}
-            justifyContent="end"
-            alignItems="center"
-            sx={{
-              width: "100%",
-              minHeight: "400px",
-              height: { xs: "50vh", md: "100vh" },
-            }}
-          >
-            <BouncingArrow
-              text="Voir plus"
-              scrollTo={scrollTo}
-              refForScroll={refForScroll}
-            />
-          </Stack>
-        ) : null}
-      </Stack>
+      <WebsiteFocusPart refsForScroll={refsForScroll} />
 
-      <ScrollToTopBtn refForScroll={topRef} />
+      <ContactSection defaultService="website" />
+
+      <ScrollToTopBtn refForScroll={refsForScroll.top} />
 
       <Footer />
     </>
