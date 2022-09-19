71c3d88e44c23a0b972b5bc13354e404e6071dd7
Quentin HEBERT <hebertquentin@aol.fr>
2022-09-19 21:54:12 +0200

    add logout page

---
+++ pages/logout.js
@@ -0,0 +1,46 @@
+import { useRouter } from "next/router"
+import { useContext, useEffect } from "react"
+import HtmlHead from "../components/ReusableComponents/page-builder/html-head"
+import PageRoot from "../components/ReusableComponents/page-builder/page-root"
+import { defaultConfig } from "../config/defaultConfig"
+import { UserContext } from "../contexts/UserContext"
+import { logout } from "../services/utils"
+
+export default function LogoutPage() {
+  // Main meta tags
+  const title = `${defaultConfig.websiteName} | Déconnexion`
+  const description = "Page de déconnexion"
+  const canonicalUrl = `${defaultConfig.webclientUrl}/logout`
+
+  // SEO helpers
+  const follow = false
+
+  // OpenGraph additional tags (sharing)
+  const type = "website"
+  const ogImg = "/medias/ogimg.png"
+
+  // Check if user is logged in
+  const { user } = useContext(UserContext)
+
+  const router = useRouter()
+
+  useEffect(() => {
+    if (user) {
+      logout()
+    }
+    router.push("/")
+  }, [user, router])
+
+  return (
+    <PageRoot>
+      <HtmlHead
+        title={title}
+        canonicalUrl={canonicalUrl}
+        description={description}
+        follow={follow}
+        type={type}
+        ogImg={ogImg}
+      />
+    </PageRoot>
+  )
+}
