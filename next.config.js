5b0c77ba1ba6e588b43e1d09504e6ce863d0dc0d
Quentin HEBERT <hebertquentin@aol.fr>
2022-10-06 04:22:40 +0200

    test caching with next js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,38 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withPlugins = require("next-compose-plugins")
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
+module.exports = withPlugins([
+  [withBundleAnalyzer({})],
+  {
+    images: {
+      domains: [
+        "creaqor.com",
+        "staging.quentinhebert.com",
+        "quentinhebert.com",
+      ],
+      minimumCacheTTL: 600,
+    },
+    headers: async () => {
+      return [
+        {
+          source: "/:pages*",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "private, max-age=180, must-revalidate",
+            },
+          ],
+        },
+      ]
+    },
+  },
+])
