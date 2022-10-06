2b891167642666efca81f0d33ed91216fc7650fd
Quentin HEBERT <hebertquentin@aol.fr>
2022-10-06 05:05:45 +0200

    Revert "test caching with next js"
    This reverts commit 5b0c77ba1ba6e588b43e1d09504e6ce863d0dc0d.

--- next.config.js
+++ next.config.js
@@ -1,38 +1,6 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
 
-// module.exports = nextConfig
-
-const withPlugins = require("next-compose-plugins")
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-})
-
-module.exports = withPlugins([
-  [withBundleAnalyzer({})],
-  {
-    images: {
-      domains: [
-        "creaqor.com",
-        "staging.quentinhebert.com",
-        "quentinhebert.com",
-      ],
-      minimumCacheTTL: 600,
-    },
-    headers: async () => {
-      return [
-        {
-          source: "/:all*(svg|jpg|png)",
-          headers: [
-            {
-              key: "Cache-Control",
-              value: "public, max-age=9999999999, must-revalidate",
-            },
-          ],
-        },
-      ]
-    },
-  },
-])
+module.exports = nextConfig
