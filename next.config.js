37661ddad64a6aafe8e115a76dee6a66a31cccee
Quentin HEBERT <hebertquentin@aol.fr>
2022-10-17 17:38:05 +0200

    cache with next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  headers: async () => [
+    {
+      source: "/:all*(svg|jpg|png|jpeg)",
+      locale: false,
+      headers: [
+        {
+          key: "Cache-Control",
+          value: "public, max-age=31536000, immutable",
+        },
+      ],
+    },
+  ],
 }
 
 module.exports = nextConfig
