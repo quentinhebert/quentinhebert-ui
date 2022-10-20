60e4310af122021b216f7abd4b1452e3b2feb05a
Quentin HEBERT <hebertquentin@aol.fr>
2022-10-20 12:28:10 +0200

    move file reset password

---
+++ pages/account/reset-password/[token].js
@@ -0,0 +1,34 @@
+import React from "react"
+import ResetPassordLayout from "../../../components/Layouts/ResetPasswordLayout"
+import Footer from "../../../components/Navigation/Footers/Footer"
+import HtmlHead from "../../../components/ReusableComponents/page-builder/html-head"
+import PageRoot from "../../../components/ReusableComponents/page-builder/page-root"
+
+// Main meta tags
+const title = "Réinitialisation de mot de passe"
+const description = `Réinitialisez votre mot de passe`
+
+// SEO helpers
+const follow = false
+
+// OpenGraph additional tags (sharing)
+const type = "website"
+const ogImg = "/medias/ogimg.png"
+
+export default function ResetPassordPage() {
+  return (
+    <PageRoot>
+      <HtmlHead
+        title={title}
+        description={description}
+        follow={follow}
+        type={type}
+        ogImg={ogImg}
+      />
+
+      <ResetPassordLayout />
+
+      <Footer />
+    </PageRoot>
+  )
+}
