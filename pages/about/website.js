53dd91c083036381d93b9cfe039cab97e91315c7
Quentin HEBERT <hebertquentin@aol.fr>
2022-10-22 12:53:54 +0200

    update layout of page about website

--- pages/about/website.js
+++ pages/about/website.js
@@ -1,48 +1,28 @@
-import Footer from "../../components/Navigation/Footers/Footer"
 import AboutWebsiteLayout from "../../components/Layouts/AboutWebsiteLayout"
-import PageRoot from "../../components/ReusableComponents/page-builder/page-root"
-import HtmlHead from "../../components/ReusableComponents/page-builder/html-head"
-import { motion } from "framer-motion"
 import prepareProps from "../../services/public-fetchers"
+import PagesLayout from "../../components/Layouts/PagesLayout"
 
-export default function AboutWebsitePage({ footer }) {
+const head = {
   // Main meta tags
-  const title = "À propos du site"
-  const description = "Découvrez tout ce qu'il faut savoir sur ce site web !"
-
+  title: "À propos du site",
+  description: "Découvrez tout ce qu'il faut savoir sur ce site web !",
   // SEO helpers
-  const follow = true
-  const keywords =
-    "Web developper, développeur web, javascript, informations, à propos du site, technologies, web services"
-
+  follow: true,
+  keywords:
+    "Filmmaker, Filmmaking, Videomaker, editor, content creator, wedding filmmaker, belgium, advertising, corporate videos, corporate filmmaking, Web developper, développeur web, javascript, informations, à propos du site, technologies, web services",
   // OpenGraph additional tags (sharing)
-  const type = "website"
-  const ogImg = "/medias/ogimg.png"
+  type: "website",
+  ogImg: "/medias/ogimg.png",
+}
 
+export default function AboutWebsitePage({ navbar, footer }) {
   return (
-    <motion.div
-      exit={{ opacity: 0 }}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-    >
-      <PageRoot>
-        <HtmlHead
-          title={title}
-          description={description}
-          follow={follow}
-          type={type}
-          ogImg={ogImg}
-          keywords={keywords}
-        />
-
-        <AboutWebsiteLayout />
-
-        <Footer staticData={footer} />
-      </PageRoot>
-    </motion.div>
+    <PagesLayout head={head} navbarData={navbar} footerData={footer}>
+      <AboutWebsiteLayout />
+    </PagesLayout>
   )
 }
 
 export async function getStaticProps() {
-  return await prepareProps(["footer"])
+  return await prepareProps(["navbar", "footer"])
 }
