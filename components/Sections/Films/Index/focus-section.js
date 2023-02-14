6b3f155cd17846bacfda52233f9e4001583fb0ec
Quentin HEBERT <hebertquentin@aol.fr>
2023-02-14 13:37:18 +0100

    add slide anim

--- components/Sections/Films/Index/focus-section.js
+++ components/Sections/Films/Index/focus-section.js
@@ -36,16 +36,15 @@ export default function FocusSection(props) {
   const variants = {
     title: {
       visible: {
-        y: 0,
         opacity: 1,
         transition: { duration: 1 },
       },
-      hidden: { opacity: 0, y: -20 },
+      hidden: { opacity: 0 },
     },
     text: {
       visible: {
         opacity: 1,
-        transition: { duration: 1, delay: 1 },
+        transition: { duration: 1, delay: 0.75 },
       },
       hidden: { opacity: 0 },
     },
@@ -91,63 +90,66 @@ export default function FocusSection(props) {
           marginTop: "0.01px",
         }}
       >
-        <Stack
-          width="100%"
-          alignItems="end"
-          sx={{ padding: { xs: "2rem", md: "4rem" } }}
-        >
-          <motion.div
-            initial="hidden"
-            variants={variants.title}
-            animate={controls}
-            style={{
-              width: md ? "60%" : "70%",
-              display: "flex",
-              flexDirection: "column",
-              gap: "1.5rem",
-            }}
+        <Slide direction="left" {...{ timeout: 2000 }} in={show}>
+          <Stack
+            width="100%"
+            alignItems="end"
+            sx={{ padding: { xs: "2rem", md: "4rem" } }}
           >
-            <Typography
-              variant="h2"
-              textAlign="center"
-              color="secondary"
-              sx={{
-                fontSize: { xs: "4rem", md: "5rem" },
-                textShadow: "0px 0px 20px rgb(0,0,0,0.5)",
+            <motion.div
+              initial="hidden"
+              variants={variants.title}
+              animate={controls}
+              style={{
+                width: md ? "60%" : "70%",
+                display: "flex",
+                flexDirection: "column",
+                gap: "1.5rem",
               }}
             >
-              Focus
-            </Typography>
-          </motion.div>
+              <Typography
+                variant="h2"
+                textAlign="center"
+                color="secondary"
+                sx={{
+                  fontSize: { xs: "4rem", md: "5rem" },
+                  textShadow: "0px 0px 20px rgb(0,0,0,0.5)",
+                }}
+              >
+                Focus
+              </Typography>
+            </motion.div>
 
-          <motion.div
-            initial="hidden"
-            variants={variants.text}
-            animate={controls}
-            style={{
-              width: md ? "60%" : "70%",
-              display: "flex",
-              flexDirection: "column",
-              gap: "1.5rem",
-            }}
-          >
-            <BodyText textAlign="center" color="#fff" fontWeight="bold">
-              <Keyword text="Passionné" /> depuis {expNbYears} années, j'ai fait
-              de la vidéo mon métier car c'est grâce au son et à l'image que je
-              parviens à m'exprimer avec le plus de <Keyword text="sincérité" />
-              .
-              <p />
-              J'ai appris en <Keyword text="autodidacte" />, poussé par l'envie
-              de créer.
-            </BodyText>
+            <motion.div
+              initial="hidden"
+              variants={variants.text}
+              animate={controls}
+              style={{
+                width: md ? "60%" : "70%",
+                display: "flex",
+                flexDirection: "column",
+                gap: "1.5rem",
+              }}
+            >
+              <BodyText textAlign="center" color="#fff" fontWeight="bold">
+                <Keyword text="Passionné" /> depuis {expNbYears} années, j'ai
+                fait de la vidéo mon métier car c'est grâce au son et à l'image
+                que je parviens à m'exprimer avec le plus de{" "}
+                <Keyword text="sincérité" />
+                .
+                <p />
+                J'ai appris en <Keyword text="autodidacte" />, poussé par
+                l'envie de créer.
+              </BodyText>
 
-            <Box sx={{ textAlign: "center" }}>
-              <PillButton onClick={(e) => scrollTo(refsForScroll.portfolio)}>
-                Voir mes réalisations
-              </PillButton>
-            </Box>
-          </motion.div>
-        </Stack>
+              <Box sx={{ textAlign: "center" }}>
+                <PillButton onClick={(e) => scrollTo(refsForScroll.portfolio)}>
+                  Voir mes réalisations
+                </PillButton>
+              </Box>
+            </motion.div>
+          </Stack>
+        </Slide>
 
         <Slide direction="right" {...{ timeout: 2000 }} in={show}>
           <Box
