4ae4dc4430085c209f3f46ec13d0bc64aa69631b
Quentin HEBERT <hebertquentin@aol.fr>
2023-02-14 13:41:05 +0100

    add animation to contact section

--- components/Sections/ReusableSections/contact-section.js
+++ components/Sections/ReusableSections/contact-section.js
@@ -1,11 +1,9 @@
 import { useEffect } from "react"
-import { Box, Stack, Typography } from "@mui/material"
+import { Stack, Typography } from "@mui/material"
 import ContactForm from "../../Forms/contact-form"
 import { useAnimation, motion } from "framer-motion"
 import { useInView } from "react-intersection-observer"
 import CenteredMaxWidthContainer from "../../Containers/centered-max-width-container"
-import StrokeText from "../../Text/stroke-text"
-import styles from "../../../styles/TextShine.module.css"
 
 export default function ContactSection(props) {
   const { defaultService } = props
@@ -14,10 +12,11 @@ export default function ContactSection(props) {
   const [ref, inView] = useInView()
   const variants = (key) => ({
     visible: {
+      x: 0,
       opacity: 1,
-      transition: { duration: 0.75, delay: 0 },
+      transition: { duration: 0.75, delay: key },
     },
-    hidden: { opacity: 0 },
+    hidden: { opacity: 0, x: -25 },
   })
   const controls = useAnimation()
 
@@ -45,13 +44,15 @@ export default function ContactSection(props) {
       ref={ref}
     >
       <CenteredMaxWidthContainer pixels="800px" percents="80%" gap={2}>
-        <Typography variant="h2" color="secondary">
-          Vous avez un projet...
-        </Typography>
+        <motion.div initial="hidden" variants={variants(0)} animate={controls}>
+          <Typography variant="h2" color="secondary">
+            Vous avez un projet...
+          </Typography>
+        </motion.div>
 
         <motion.div
           initial="hidden"
-          variants={variants(1)}
+          variants={variants(0.5)}
           animate={controls}
           style={{ width: "100%" }}
         >
