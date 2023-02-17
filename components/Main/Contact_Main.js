4d70a13e56b87004bbc7c7f6fc9d2f923e031c60
Quentin HEBERT <hebertquentin@aol.fr>
2023-02-17 16:42:22 +0100

    fix contact page

--- components/Main/Contact_Main.js
+++ components/Main/Contact_Main.js
@@ -6,71 +6,44 @@ import ContactForm from "../Forms/contact-form"
 import { useEffect } from "react"
 import CenteredMaxWidthContainer from "../Containers/centered-max-width-container"
 import FixedBackground from "../Backgrounds/fixed-background"
+import MotionDivOnMount from "../Animation/motion-div-on-mount"
 
 export default function Contact_Main(props) {
   const { staticData } = props
 
-  /********** ANIMATION **********/
-  const [ref, inView] = useInView()
-  const controls = useAnimation()
-  const variants = (xOrigin) => ({
-    visible: {
-      opacity: 1,
-      x: 0,
-      transition: { duration: 1 },
-    },
-    hidden: { opacity: 0, xOrigin },
-  })
-  useEffect(() => {
-    if (inView) controls.start("visible")
-    else controls.start("hidden")
-  }, [controls, inView])
-
-  /********** ANIMATION STYLE **********/
-  const fullScreen = useMediaQuery((theme) => theme.breakpoints.down("md"))
-  const motionDivStyle = {
-    width: fullScreen ? "100%" : "50%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    marginBottom: "2rem",
-  }
-
   return (
-    <Stack ref={ref} overflow="hidden" sx={{ marginTop: "-82px" }}>
-      <FixedBackground
-        background={(theme) =>
-          // `linear-gradient(150deg, ${theme.palette.background.main} 0%, rgb(0,0,0,1) 80%)`
-          `#000`
-        }
-      />
+    <Stack overflow="hidden" sx={{ marginTop: "-82px" }}>
+      <FixedBackground background="#000" />
 
       <CenteredMaxWidthContainer pixels="1200px" percents="80%">
         <Stack
           alignItems="start"
           margin="calc(65px + 4rem) 0"
           sx={{
-            gap: { xs: 8, md: 0 },
-            flexDirection: { xs: "column", md: "row" },
+            gap: { xs: 8, lg: 0 },
+            flexDirection: { xs: "column", lg: "row" },
           }}
         >
-          <motion.div
-            initial="hidden"
-            variants={variants(-200)}
-            animate={controls}
-            style={motionDivStyle}
+          <MotionDivOnMount
+            visible={{
+              opacity: 1,
+              x: 0,
+            }}
+            hidden={{ opacity: 0, x: -200 }}
           >
             <ContactInformationSection staticData={staticData} />
-          </motion.div>
+          </MotionDivOnMount>
 
-          <motion.div
-            initial="hidden"
-            variants={variants(200)}
-            animate={controls}
-            style={motionDivStyle}
+          <MotionDivOnMount
+            visible={{
+              opacity: 1,
+              x: 0,
+            }}
+            hidden={{ opacity: 0, x: 200 }}
+            delay={0.15}
           >
             <ContactForm defaultDirection="column" />
-          </motion.div>
+          </MotionDivOnMount>
         </Stack>
       </CenteredMaxWidthContainer>
     </Stack>
