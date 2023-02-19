873c38bd9f72b51727ed74cb48ec20c688cac7e4
Quentin HEBERT <hebertquentin@aol.fr>
2023-02-19 18:39:25 +0100

    about page seo

--- pages/about/index.js
+++ pages/about/index.js
@@ -5,7 +5,8 @@ import PagesLayout from "../../components/Layouts/PagesLayout"
 const head = {
   // Main meta tags
   title: "À propos",
-  description: "Quentin Hébert | À propos",
+  description:
+    "Je m'appelle Quentin, et je suis vidéaste professionnel et développeur web en freelance. Je réalise des films pour vos mariages, des clips, des publicités et bien plus encore ! Si vous cherchez un site web professionnel, contactez-moi !",
   // SEO helpers
   follow: true,
   keywords:
