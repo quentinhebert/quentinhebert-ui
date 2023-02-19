03b076e4848d4f91605a0860a9d21dc130958f04
Quentin HEBERT <hebertquentin@aol.fr>
2023-02-19 18:32:28 +0100

    fix

--- components/Helpers/html-head.js
+++ components/Helpers/html-head.js
@@ -23,21 +23,18 @@ export default function HtmlHead({
       <title>{pageTitle}</title>
       <meta name="description" content={description} />
       <link rel="icon" href="/favicon.ico" />
-
       <meta
         name="robots"
         content={follow ? "index, follow" : "noindex, nofollow"}
       />
       <link rel="canonical" href={canonicalUrl} />
-
-      {<meta name="keywords" content={keywords || ""} />}
-
+      keywords
+      <meta name="keywords" content={keywords || ""} />
       <meta property="og:url" content={canonicalUrl} />
       <meta property="og:type" content={type} />
       <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImg} />
-
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
   )
