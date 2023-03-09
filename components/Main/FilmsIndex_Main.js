108e3a233d2f5109ce1f840a419885477d8d1429
Quentin HEBERT <hebertquentin@aol.fr>
2023-03-09 23:14:42 +0100

    try to fix timeout on server

--- components/Main/FilmsIndex_Main.js
+++ components/Main/FilmsIndex_Main.js
@@ -1,26 +1,30 @@
 import React, { useRef } from "react"
 import HeroSection from "../Sections/Films/Index/hero-section"
 import FocusSection from "../Sections/Films/Index/focus-section"
-import dynamic from "next/dynamic"
 
-const ParallaxGaleryCTA = dynamic(
-  () => import("../Sections/parallax-galery-cta"),
-  {
-    ssr: false,
-  }
-)
-const ContactSection = dynamic(
-  () => import("../Sections/ReusableSections/contact-section"),
-  {
-    ssr: false,
-  }
-)
-const PortfolioSection = dynamic(
-  () => import("../Sections/Films/Index/Portfolio/portfolio-section"),
-  {
-    ssr: false,
-  }
-)
+import ParallaxGaleryCTA from "../Sections/parallax-galery-cta"
+import ContactSection from "../Sections/ReusableSections/contact-section"
+import PortfolioSection from "../Sections/Films/Index/Portfolio/portfolio-section"
+
+// import dynamic from "next/dynamic"
+// const ParallaxGaleryCTA = dynamic(
+//   () => import("../Sections/parallax-galery-cta"),
+//   {
+//     ssr: false,
+//   }
+// )
+// const ContactSection = dynamic(
+//   () => import("../Sections/ReusableSections/contact-section"),
+//   {
+//     ssr: false,
+//   }
+// )
+// const PortfolioSection = dynamic(
+//   () => import("../Sections/Films/Index/Portfolio/portfolio-section"),
+//   {
+//     ssr: false,
+//   }
+// )
 
 export default function FilmsIndexLayout(props) {
   const {} = props
