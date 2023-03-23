5f3531fa3c738b20266325ba6c5b1bdc30f2686f
Quentin HEBERT <hebertquentin@aol.fr>
2023-03-23 18:12:45 +0100

    update UI payment succeeded

--- pages/account/orders/[id]/checkout/success.js
+++ pages/account/orders/[id]/checkout/success.js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router"
 import PillButton from "../../../../../components/Buttons/pill-button"
 import PagesLayout from "../../../../../components/Layouts/PagesLayout"
 import BodyText from "../../../../../components/Text/body-text"
+import Span from "../../../../../components/Text/span"
 import PageTitle from "../../../../../components/Titles/page-title"
 
 const head = {
@@ -21,9 +22,18 @@ export default function SuccessPaymentPage() {
 
   return (
     <PagesLayout head={head}>
-      <Stack flexGrow={1} className="flex-center">
+      <Stack flexGrow={1} className="flex-center" minHeight="400px">
         <Stack textAlign="center" gap={4}>
-          <PageTitle text="Paiement réussi ✅" />
+          <PageTitle
+            textAlign="center"
+            text={
+              <>
+                Paiement réussi
+                <br />
+                <Span fontSize="0.5em">✅</Span>
+              </>
+            }
+          />
           <BodyText textAlign="center">Merci pour votre paiement !</BodyText>
           <PillButton onClick={() => router.push("/account/orders")}>
             Mes commandes
