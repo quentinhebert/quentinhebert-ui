cac18c7ca279802bcd53b71fdbe50cfc047474ca
Quentin HEBERT <hebertquentin@aol.fr>
2023-04-12 19:34:40 +0200

    start to create translation file

---
+++ services/translation.js
@@ -0,0 +1,330 @@
+const translations = {
+  footer: {
+    termsOfUse: {
+      fr: "CGU",
+      en: "Terms of Use",
+    },
+    termsAndConditions: {
+      fr: "Mentions légales et CGV",
+      en: "Terms and Conditions",
+    },
+  },
+  contactForm: {
+    wordCarousel: {
+      text: {
+        fr: "...et vous cherchez un ",
+        en: "...and you are looking for a ",
+      },
+      words: {
+        films: {
+          fr: ["vidéaste", "réalisateur", "cadreur", "monteur"],
+          en: ["filmmaker", "director", "camera operator", "editor"],
+        },
+        websites: {
+          fr: [
+            "developpeur front-end",
+            "developpeur back-end",
+            "web designer",
+            "super cool guy",
+          ],
+          en: [
+            "frontend developer",
+            "backend developer",
+            "web-designer",
+            "mec super sympa",
+          ],
+        },
+      },
+    },
+    annotation: {
+      fr: "Laissez-moi un message",
+      en: "Leave me a message",
+    },
+    optional: {
+      fr: "Optionnel",
+      en: "Optional",
+    },
+    firstname: {
+      label: {
+        fr: "Prénom",
+        en: "Firstname",
+      },
+      helperText: {
+        fr: "Veuillez remplir ce champ",
+        en: "Please fullfil this field",
+      },
+      placeholderVideo: {
+        fr: "",
+        en: "",
+      },
+      placeholderWeb: {
+        fr: "",
+        en: "",
+      },
+      placeholderMix: {
+        fr: "",
+        en: "",
+      },
+    },
+    lastname: {
+      label: {
+        fr: "Nom",
+        en: "Lastname",
+      },
+      placeholderVideo: {
+        fr: "",
+        en: "",
+      },
+      placeholderWeb: {
+        fr: "",
+        en: "",
+      },
+      placeholderMix: {
+        fr: "",
+        en: "",
+      },
+    },
+    email: {
+      label: {
+        fr: "E-mail",
+        en: "Email",
+      },
+      helperText: {
+        fr: "Cet e-mail n'est pas valide",
+        en: "This email is not valid",
+      },
+      placeholderVideo: {
+        fr: "",
+        en: "",
+      },
+      placeholderWeb: {
+        fr: "",
+        en: "",
+      },
+      placeholderMix: {
+        fr: "",
+        en: "",
+      },
+    },
+    phone: {
+      label: {
+        fr: "Téléphone",
+        en: "Phone",
+      },
+      placeholder: {
+        fr: "",
+        en: "",
+      },
+    },
+    company: {
+      label: {
+        fr: "Entreprise",
+        en: "Company",
+      },
+      placeholderVideo: {
+        fr: "",
+        en: "",
+      },
+      placeholderWeb: {
+        fr: "",
+        en: "",
+      },
+      placeholderMix: {
+        fr: "",
+        en: "",
+      },
+    },
+    budget: {
+      fr: "Mon budget",
+      en: "My budget",
+    },
+    description: {
+      label: {
+        fr: "À propos de mon projet",
+        en: "About my project",
+      },
+      placeholderVideo: {
+        fr: "",
+        en: "",
+      },
+      placeholderWeb: {
+        fr: "",
+        en: "",
+      },
+      placeholderMix: {
+        fr: "",
+        en: "",
+      },
+    },
+    btn: {
+      fr: "Envoyer",
+      en: "Submit",
+    },
+  },
+  homepage: {
+    hero: {
+      activity1: {
+        fr: "Réalisateur",
+        en: "Director",
+      },
+      activity2: {
+        fr: "Développeur web",
+        en: "Web developer",
+      },
+      btn1: {
+        fr: "Vidéo",
+        en: "Video",
+      },
+      btn2: {
+        fr: "Web",
+        en: "Web",
+      },
+    },
+    introVideo: {
+      title: {
+        pt1: {
+          fr: "Une ",
+          en: "A ",
+        },
+        pt2: {
+          fr: "vidéo ",
+          en: "video ",
+        },
+        pt3: {
+          fr: "vaut mille mots.",
+          en: "is worth a thousand words.",
+        },
+      },
+    },
+    welcome: {
+      title: {
+        pt1: {
+          fr: "Créons ",
+          en: "Let's create ",
+        },
+        pt2: {
+          fr: "ensemble, ",
+          en: "together, ",
+        },
+        pt3: {
+          fr: "une histoire ",
+          en: "a story ",
+        },
+        pt4: {
+          fr: "qui vous ressemble",
+          en: "which feels like you",
+        },
+      },
+      text: {
+        pt1: {
+          fr: `Je m'appelle Quentin, j'ai `,
+          en: "I'm Quentin, I'm",
+        },
+        pt2: {
+          fr: "ans et je suis vidéaste professionnel et développeur web en freelance. ",
+          en: "years old and I am a professional filmmaker and freelance web developer.",
+        },
+        pt3: {
+          fr: `Donnons du sens à vos idées, que ce soit en vidéo ou sur le web, et réalisons ensemble un film ou un site qui vous ressemble.`,
+          en: "Let's give meaning to your ideas, as much with a video as on the web, and let's create a film or a website that feels like you.",
+        },
+      },
+      btn: {
+        fr: "Contactez-moi",
+        en: "Contact me",
+      },
+    },
+    services: {
+      title: {
+        fr: "Les services que je propose",
+        en: "The services I provide",
+      },
+    },
+    references: {
+      title: {
+        fr: "Ils m'ont fait confiance",
+        en: "They've trust me",
+      },
+      subtitle: {
+        fr: "Merci à eux !",
+        en: "Thanks to them !",
+      },
+    },
+  },
+  films: {
+    hero: {
+      title: {
+        fr: "Vidéaste freelance",
+        en: "Freelance filmmaker",
+      },
+      iam: {
+        fr: "Je suis",
+        en: "I am",
+      },
+      job1: {
+        fr: "Réalisateur",
+        en: "Director",
+      },
+      job2: {
+        fr: "Cadreur",
+        en: "Camera operator",
+      },
+      job3: {
+        fr: "Monteur",
+        en: "Editor",
+      },
+      btn: {
+        fr: "Bande démo",
+        en: "Showreel",
+      },
+    },
+    CTA: {
+      title: {
+        fr: "Ensemble, donnons vie à vos projets.",
+        en: "Together, let's make your projects come true.",
+      },
+      btn: {
+        fr: "Contactez-moi",
+        en: "Contact me",
+      },
+    },
+    portfolio: {
+      title: {
+        fr: "Mes réalisations",
+        en: "My work",
+      },
+      categories: {
+        all: {
+          fr: "Tout",
+          en: "All",
+        },
+        event: {
+          fr: "Événementiel",
+          en: "Events",
+        },
+        shortFilms: {
+          fr: "Court-métrage",
+          en: "Short films",
+        },
+        clips: {
+          fr: "Clip",
+          en: "Music clip",
+        },
+        corporate: {
+          fr: "Entreprise",
+          en: "Corporate",
+        },
+        sport: {
+          fr: "Sport",
+          en: "Sport",
+        },
+      },
+      seeMore: {
+        fr: "Afficher plus",
+        en: "Show more",
+      },
+    },
+  },
+}
+
+export default translations
