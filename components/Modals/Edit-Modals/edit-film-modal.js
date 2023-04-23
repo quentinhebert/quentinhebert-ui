03687f4c0f52e6f3fdd08cbf7eb2fa96603b74a5
Quentin HEBERT <hebertquentin@aol.fr>
2023-04-23 21:27:14 +0200

    change system in backoffice

--- components/Modals/Edit-Modals/edit-film-modal.js
+++ components/Modals/Edit-Modals/edit-film-modal.js
@@ -316,7 +316,7 @@ function EditFilmModal(props) {
                         .length === 1) ||
                     false
                   }
-                  label={item.label}
+                  label={item.label.fr}
                   labelcolor="#fff"
                   fontSize="0.9rem"
                   onChange={handleChangeMultipleCheckbox("gear", item)}
