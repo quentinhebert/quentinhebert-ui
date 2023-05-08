df041e7c43e535e4f52109b912c47fe31d04568f
Quentin HEBERT <hebertquentin@aol.fr>
2023-05-08 21:50:52 +0200

    add hosting servcie terms

--- components/Sections/TermsAndConditions/terms-and-conditions-section.js
+++ components/Sections/TermsAndConditions/terms-and-conditions-section.js
@@ -35,14 +35,27 @@ export default function TermsAndConditionsSection({ staticData }) {
         <Typography fontWeight="bold">{data.terms.terms_fullname}</Typography>
         <Typography>{data.terms.terms_email}</Typography>
         <Typography>{data.terms.terms_phone}</Typography>
-        <Typography>{data.terms.terms_line1}</Typography>
-        <Typography>{data.terms.terms_line2}</Typography>
         <Typography>
-          {data.terms.terms_postal_code} {data.terms.terms_city},{" "}
+          {data.terms.terms_line1 ? `${data.terms.terms_line1}, ` : null}
+          {data.terms.terms_line2 ? `${data.terms.terms_line2}, ` : null}
+          {data.terms.terms_postal_code
+            ? `${data.terms.terms_postal_code} `
+            : null}
+          {data.terms.terms_city ? `${data.terms.terms_city}, ` : " "}
           {data.terms.terms_country}
         </Typography>
         <Typography>SIRET {data.terms.siret}</Typography>
         <Typography>RCS {data.terms.rcs}</Typography>
+        <p />
+        <Typography>Hébergeur du Site :</Typography>
+        <Typography>
+          <strong>Netlify, Inc.</strong>
+        </Typography>
+        <Typography>
+          <em>
+            44 Montgomery Street, Suite 300, San Francisco, California 94104
+          </em>
+        </Typography>
       </Stack>
 
       <Stack>
