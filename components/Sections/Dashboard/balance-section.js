cabf1e3c150112106bf1e16ef9c1a3cdec1383ac
Quentin HEBERT <hebertquentin@aol.fr>
2023-06-05 20:18:13 +0200

    initiate payout

--- components/Sections/Dashboard/balance-section.js
+++ components/Sections/Dashboard/balance-section.js
@@ -10,6 +10,7 @@ import { formatPrice } from "../../../services/utils"
 
 export default function BalanceSection({}) {
   const [balance, setBalance] = useState(null)
+  const [lastPayout, setLastPayout] = useState(null)
 
   const fetchBalance = async () => {
     const res = await apiCall.dashboard.balance.get()
@@ -18,8 +19,24 @@ export default function BalanceSection({}) {
       setBalance(jsonRes)
     } else alert("error")
   }
+  const fetchLastPayout = async () => {
+    const res = await apiCall.dashboard.payouts.getLast()
+    if (res && res.ok) {
+      const jsonRes = await res.json()
+      setLastPayout(jsonRes)
+    } else alert("error")
+  }
 
-  useEffect(() => fetchBalance(), [])
+  const initiatePayout = async () => {
+    const res = await apiCall.dashboard.payouts.initiate()
+    if (res && res.ok) alert("Succès")
+    else alert("error")
+  }
+
+  useEffect(() => {
+    fetchBalance()
+    fetchLastPayout()
+  }, [])
 
   return (
     <CustomCard
@@ -56,7 +73,7 @@ export default function BalanceSection({}) {
             </Grid>
             <Grid item xs={4} display="flex" justifyContent="end">
               <BodyText textAlign="right">
-                {balance?.available[0].amount || 0} €
+                {formatPrice(balance?.available[0].amount || 0)} €
               </BodyText>
             </Grid>
             <Grid item xs={8} display="flex" alignItems="start">
@@ -67,11 +84,28 @@ export default function BalanceSection({}) {
                 {formatPrice(balance?.pending[0].amount || 0)} €
               </BodyText>
             </Grid>
+            <Grid item xs={8} display="flex" alignItems="start">
+              <BodyText color="grey">
+                Dernier virement vers votre compte (status: {lastPayout?.status}
+                )
+              </BodyText>
+            </Grid>
+            <Grid item xs={4} display="flex" justifyContent="end">
+              <BodyText textAlign="right">
+                {formatPrice(lastPayout?.amount || 0)} €
+              </BodyText>
+            </Grid>
           </Grid>
         </Stack>
 
         <Stack maxWidth="300px" width="100%">
-          <PillButton endIcon={<EastIcon />}>Virer l'argent</PillButton>
+          <PillButton
+            disabled={balance?.available[0].amount <= 0}
+            onClick={initiatePayout}
+            endIcon={<EastIcon />}
+          >
+            Virer l'argent
+          </PillButton>
         </Stack>
       </Stack>
     </CustomCard>
--- services/apiCalls/dashboard.js
+++ services/apiCalls/dashboard.js
@@ -33,6 +33,34 @@ const dashboard = {
       }
     },
   },
+  payouts: {
+    initiate: async () => {
+      try {
+        return await fetch(`${defaultConfig.apiUrl}/dashboard/payouts`, {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${await getFreshToken()}`,
+            "Content-Type": "application/json",
+          },
+        })
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    getLast: async () => {
+      try {
+        return await fetch(`${defaultConfig.apiUrl}/dashboard/payouts/last`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${await getFreshToken()}`,
+            "Content-Type": "application/json",
+          },
+        })
+      } catch (err) {
+        console.error(err)
+      }
+    },
+  },
   kpi: {
     get: async () => {
       try {
