6ade80817c074f25495b306a0e397061138deeb1
Quentin HEBERT <hebertquentin@aol.fr>
2023-06-29 18:18:42 +0200

    text with dengerous inner html

--- components/Helpers/html-head.js
+++ components/Helpers/html-head.js
@@ -59,6 +59,34 @@ export default function HtmlHead({
       <meta property="og:description" content={description} />
       <meta property="og:image" content={ogImg} />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.fbAsyncInit = function () {
+                window.FB.init({
+                    appId: ${defaultConfig.facebookAppId},
+                    cookie: true,
+                    xfbml: true,
+                    version: 'v8.0'
+                });
+    
+                // auto authenticate with the api if already logged in with facebook
+                window.FB.getLoginStatus(({ authResponse }) => {
+                    if (authResponse) {
+                        accountService.apiAuthenticate(authResponse.accessToken).then(resolve);
+                    }
+                });
+            };
+    
+            // load facebook sdk script
+            (function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) { return; }
+                js = d.createElement(s); js.id = id;
+                js.src = "https://connect.facebook.net/en_US/sdk.js";
+                fjs.parentNode.insertBefore(js, fjs);
+            }(document, 'script', 'facebook-jssdk'));    `,
+        }}
+      />
     </Head>
   )
 }
