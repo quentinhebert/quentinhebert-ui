fef6457a020897b5803bfe4a8682ffd531b30a62
Quentin HEBERT <hebertquentin@aol.fr>
2023-06-30 09:59:52 +0200

    try with enxt script

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,13 @@
 import { Stack } from "@mui/material"
 import { Html, Head, Main, NextScript } from "next/document"
+import Script from "next/script"
 
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <Script src="../scripts/facebook-init.js" />
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ scripts/facebook-init.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function () {
+  FB.init({
+    appId: "{your-app-id}",
+    cookie: true,
+    xfbml: true,
+    version: "{api-version}",
+  })
+
+  FB.AppEvents.logPageView()
+}
+
+function initFB(d, s, id) {
+  var js,
+    fjs = d.getElementsByTagName(s)[0]
+  if (d.getElementById(id)) {
+    return
+  }
+  js = d.createElement(s)
+  js.id = id
+  js.src = "https://connect.facebook.net/en_US/sdk.js"
+  fjs.parentNode.insertBefore(js, fjs)
+}
+
+initFB(document, "script", "facebook-jssdk")
