440fc3d4d8233db204ffd5fd208fbab5090e2227
Quentin HEBERT <hebertquentin@aol.fr>
2023-06-30 10:41:22 +0200

    try again

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,33 @@
 import { Stack } from "@mui/material"
 import { Html, Head, Main, NextScript } from "next/document"
 import Script from "next/script"
+import { defaultConfig } from "../config/defaultConfig"
 
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <Script src="/facebook-init.js" />
-      </Head>
+      <Head></Head>
       <body>
         <Main />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.fbAsyncInit = function() {
+        FB.init({
+          appId  : ${defaultConfig.facebookAppId},
+          status : true, // check login status
+          cookie : true, // enable cookies to allow the server to access the session
+          xfbml  : true  // parse XFBML
+        });
+      };
+
+      (function() {
+        var e = document.createElement('script');
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        e.async = true;
+        document.getElementById('fb-root').appendChild(e);
+      }());`,
+          }}
+        ></script>
         <NextScript />
       </body>
     </Html>
