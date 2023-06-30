11195a7fad78c9343ce0a5d53570545e50182e3e
Quentin HEBERT <hebertquentin@aol.fr>
2023-06-30 10:23:07 +0200

    try directly in component

--- components/Forms/signup-form.js
+++ components/Forms/signup-form.js
@@ -276,6 +276,31 @@ export default function SignUpForm({
     if (!!handleClose) handleClose()
   }
 
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: defaultConfig.facebookAppId,
+        xfbml: true,
+        version: "v11.0",
+      })
+      window.FB.AppEvents.logPageView()
+    }
+
+    function initFB(d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0]
+      if (d.getElementById(id)) {
+        return
+      }
+      js = d.createElement(s)
+      js.id = id
+      js.src = "//connect.facebook.net/en_US/sdk.js"
+      fjs.parentNode.insertBefore(js, fjs)
+    }
+
+    initFB(document, "script", "facebook-jssdk")
+  }, [])
+
   /********** RENDER **********/
   return (
     <Stack gap={4}>
