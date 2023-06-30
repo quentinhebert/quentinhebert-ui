9435cf54924b0af8ff22af0145be6857b2e355e3
Quentin HEBERT <hebertquentin@aol.fr>
2023-06-30 11:05:46 +0200

    next script in main

--- pages/_document.js
+++ pages/_document.js
@@ -6,30 +6,11 @@ import { defaultConfig } from "../config/defaultConfig"
 export default function Document() {
   return (
     <Html>
-      <Head></Head>
+      <Head>
+        <Script src="/facebook-init.js" />
+      </Head>
       <body>
         <Main />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.fbAsyncInit = function() {
-        FB.init({
-          appId  : ${defaultConfig.facebookAppId},
-          status : true, // check login status
-          cookie : true, // enable cookies to allow the server to access the session
-          xfbml  : true,  // parse XFBML
-          version: v17.0
-        });
-      };
-
-      (function() {
-        var e = document.createElement('script');
-        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
-        e.async = true;
-        document.getElementById('fb-root').appendChild(e);
-      }());`,
-          }}
-        ></script>
-        <div id="fb-root"></div>
         <NextScript />
       </body>
     </Html>
