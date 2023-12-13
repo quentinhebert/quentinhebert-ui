719d3467afdb7740e056ad5e7bfc810e078b9488
Quentin HEBERT <hebertquentin@aol.fr>
2023-12-13 13:57:17 +0100

    migrate gsap welcome section animations

--- components/Sections/Homepage/welcome-section.js
+++ components/Sections/Homepage/welcome-section.js
@@ -1,15 +1,18 @@
 import { Box, Stack, Typography } from "@mui/material"
 import BodyText from "../../Text/body-text"
-import { Parallax } from "react-scroll-parallax"
 import LeftSubmitButton from "../../Buttons/left-submit-button"
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt"
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect } from "react"
 import { useAnimation, motion } from "framer-motion"
 import { useInView } from "react-intersection-observer"
 import NextLink from "../../Helpers/next-link"
 import Span from "../../Text/span"
 import { AppContext } from "../../../contexts/AppContext"
 import translations from "../../../services/translation"
+
+import gsap from "gsap"
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger"
+gsap.registerPlugin(ScrollTrigger)
 
 const birthdate = new Date("1998/1/15")
 const cur = new Date()
@@ -48,8 +51,6 @@ const GalleryImg = ({ src, ...props }) => (
 export default function WelcomeSection(props) {
   const { scrollTo, topRef, refForScroll } = props
 
-  const [progress, setProgress] = useState(0)
-
   const { lang } = useContext(AppContext)
 
   /********** ANIMATION **********/
@@ -68,12 +69,75 @@ export default function WelcomeSection(props) {
     },
   })
   useEffect(() => {
-    if (inView) {
-      controls.start("visible")
-    } else {
-      controls.start("hidden")
-    }
+    if (inView) controls.start("visible")
+    else controls.start("hidden")
   }, [controls, inView])
+
+  useEffect(() => {
+    gsap.to("#parallaxContent", {
+      yPercent: 70,
+      ease: "none",
+      scrollTrigger: {
+        spin: true,
+        trigger: "#parallaxWrapper",
+        start: "top center", // the default values
+        end:
+          "+=" +
+          (document.querySelector("#parallaxWrapper").offsetHeight + 800) +
+          " center",
+        scrub: 0.1,
+        // markers: true,
+      },
+    })
+    gsap.to("#parallaxImg1", {
+      yPercent: 0,
+      opacity: 1,
+      rotate: 10,
+      ease: "true",
+      scrollTrigger: {
+        trigger: "#parallaxWrapper",
+        start: "-400px center", // the default values
+        end:
+          "+=" +
+          (document.querySelector("#parallaxWrapper").offsetHeight + 800) +
+          " center",
+        scrub: true,
+        // markers: true,
+      },
+    })
+    gsap.to("#parallaxImg2", {
+      yPercent: -50,
+      rotate: 10,
+      opacity: 4,
+      ease: "true",
+      scrollTrigger: {
+        trigger: "#parallaxWrapper",
+        start: "-100px center", // the default values
+        end:
+          "+=" +
+          (document.querySelector("#parallaxWrapper").offsetHeight + 800) +
+          " center",
+        scrub: true,
+        // markers: true,
+      },
+    })
+    gsap.to("#parallaxImg3", {
+      yPercent: -20,
+      rotate: 10,
+      opacity: 4,
+      ease: "true",
+      scrollTrigger: {
+        trigger: "#parallaxWrapper",
+        start: "0 center",
+        end:
+          "+=" +
+          (document.querySelector("#parallaxWrapper").offsetHeight + 800) +
+          " center",
+        scrub: true,
+        // markers: true,
+      },
+    })
+  }, [])
 
   return (
     <>
@@ -95,6 +159,8 @@ export default function WelcomeSection(props) {
       >
         <Stack
           className="full-width bottom left"
+          id="parallaxWrapper"
+          mb="500px"
           sx={{
             flexDirection: { xs: "column", lg: "row" },
           }}
@@ -107,118 +173,120 @@ export default function WelcomeSection(props) {
               position: "relative",
             }}
           >
-            <Parallax
-              easing="easeInQuad"
-              scale={[1, 1.1]}
-              translateY={[5, 100]}
-              translateX={[-10, -10]}
-              rotate={["10deg", "-20deg"]}
-              style={{
-                width: "60%",
-                height: "60%",
+            <Box
+              id="parallaxImg1"
+              sx={{
+                opacity: 0,
+                rotate: "-10deg",
+                width: { xs: "50vw", lg: "30vw" },
+                height: { xs: "50vw", lg: "30vw" },
                 alignSelf: "end",
                 zIndex: 1,
               }}
             >
               <GalleryImg src="/medias/cover.jpg" />
-            </Parallax>
+            </Box>
 
-            <Parallax
-              easing="easeInQuad"
-              scale={[1, 1.6]}
-              translateY={[0, -100]}
-              translateX={[15, 15]}
-              rotate={["-10deg", "30deg"]}
-              style={{
-                width: "40%",
-                height: "40%",
+            <Box
+              id="parallaxImg2"
+              sx={{
+                position: "absolute",
+                translate: "0 100%",
+                top: 0,
+                left: "50px",
+                opacity: 0,
+                rotate: "-10deg",
+                width: { xs: "40vw", lg: "20vw" },
+                height: { xs: "40vw", lg: "20vw" },
                 zIndex: 2,
               }}
             >
               <GalleryImg src="/medias/portrait.jpg" />
-            </Parallax>
+            </Box>
 
-            <Parallax
-              easing="easeInQuad"
-              translateY={[-40, 0]}
-              scale={[1, 0.4]}
-              rotate={["0deg", "40deg"]}
-              style={{
-                width: "60%",
-                height: "60%",
-                alignSelf: "center",
+            <Box
+              id="parallaxImg3"
+              sx={{
+                position: "absolute",
+                translate: "0 100%",
+                top: 0,
+                right: "70px",
+                opacity: 0,
+                rotate: "-20deg",
+                width: { xs: "45vw", lg: "27vw" },
+                height: { xs: "45vw", lg: "27vw" },
                 zIndex: 1,
               }}
             >
               <GalleryImg src="/medias/bridge.jpg" />
-            </Parallax>
+            </Box>
           </Stack>
 
           <Stack
+            id="parallaxContent"
             sx={{
               width: { xs: "100%", lg: "50%" },
-              padding: "2rem 2rem 40vw",
+              height: "100%",
+              gap: "2rem",
+              padding: "2rem",
+              zIndex: 3,
             }}
           >
-            <Parallax translateY={["0vw", "25vw"]}>
-              <Stack gap="2rem">
-                <motion.div
-                  animate={controls}
-                  initial="hidden"
-                  variants={textVariant(0)}
-                >
-                  <Typography
-                    variant="h2"
-                    fontFamily="POPFINE"
-                    color="secondary"
-                    lineHeight={{ xs: "3rem", md: "5rem" }}
-                  >
-                    <Span sx={{ color: (theme) => theme.palette.text.white }}>
-                      {translations.homepage.welcome.title.pt1[lang]}
-                    </Span>{" "}
-                    {translations.homepage.welcome.title.pt2[lang]}{" "}
-                    <Span sx={{ color: (theme) => theme.palette.text.white }}>
-                      {translations.homepage.welcome.title.pt3[lang]}
-                    </Span>{" "}
-                    {translations.homepage.welcome.title.pt4[lang]}
-                  </Typography>
-                </motion.div>
-                <motion.div
-                  animate={controls}
-                  initial="hidden"
-                  variants={textVariant(0.5)}
-                >
-                  <BodyText preventTransition>
-                    {translations.homepage.welcome.text.pt1[lang]} {age}{" "}
-                    {translations.homepage.welcome.text.pt2[lang]}
-                    <br />
-                    <br />
-                    {translations.homepage.welcome.text.pt3[lang]}
-                  </BodyText>
-                </motion.div>
+            <motion.div
+              animate={controls}
+              initial="hidden"
+              variants={textVariant(0)}
+            >
+              <Typography
+                variant="h2"
+                fontFamily="POPFINE"
+                color="secondary"
+                lineHeight={{ xs: "3rem", md: "5rem" }}
+              >
+                <Span sx={{ color: (theme) => theme.palette.text.white }}>
+                  {translations.homepage.welcome.title.pt1[lang]}
+                </Span>{" "}
+                {translations.homepage.welcome.title.pt2[lang]}{" "}
+                <Span sx={{ color: (theme) => theme.palette.text.white }}>
+                  {translations.homepage.welcome.title.pt3[lang]}
+                </Span>{" "}
+                {translations.homepage.welcome.title.pt4[lang]}
+              </Typography>
+            </motion.div>
+            <motion.div
+              animate={controls}
+              initial="hidden"
+              variants={textVariant(0.5)}
+            >
+              <BodyText preventTransition>
+                {translations.homepage.welcome.text.pt1[lang]} {age}{" "}
+                {translations.homepage.welcome.text.pt2[lang]}
+                <br />
+                <br />
+                {translations.homepage.welcome.text.pt3[lang]}
+              </BodyText>
+            </motion.div>
 
-                <motion.div
-                  animate={controls}
-                  initial="hidden"
-                  variants={textVariant(1)}
-                >
-                  <Box width="100%">
-                    <NextLink href="/contact">
-                      <LeftSubmitButton>
-                        <Typography
-                          sx={{
-                            fontSize: { xs: ".6rem", md: ".8rem" },
-                          }}
-                        >
-                          {translations.homepage.welcome.btn[lang]}
-                        </Typography>{" "}
-                        <ArrowRightAltIcon />
-                      </LeftSubmitButton>
-                    </NextLink>
-                  </Box>
-                </motion.div>
-              </Stack>
-            </Parallax>
+            <motion.div
+              animate={controls}
+              initial="hidden"
+              variants={textVariant(1)}
+            >
+              <Box width="100%">
+                <NextLink href="/contact">
+                  <LeftSubmitButton>
+                    <Typography
+                      sx={{
+                        fontSize: { xs: ".6rem", md: ".8rem" },
+                      }}
+                    >
+                      {translations.homepage.welcome.btn[lang]}
+                    </Typography>{" "}
+                    <ArrowRightAltIcon />
+                  </LeftSubmitButton>
+                </NextLink>
+              </Box>
+            </motion.div>
           </Stack>
         </Stack>
       </Stack>
